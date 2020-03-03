365b0dae68f029c43d94c45026efd909fda5420f
Lius-ToGo <60559362+Lius-ToGo@users.noreply.github.com>
2020-03-03 16:49:30 +0800

    main

--- app.py
+++ app.py
@@ -2,14 +2,11 @@
 该脚本使用开发服务器运行应用程序。
 它包含应用程序的路由和视图的定义。
 """
-
 import bilihead
 from flask import Flask,redirect
 app = Flask(__name__)
 
-
 wsgi_app = app.wsgi_app
-
 
 @app.route('/bilihead')
 def hello():
